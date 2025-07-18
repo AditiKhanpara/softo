8a0cabca68954ba17646633de755f4388fdd22be
ridhamkakadiya1 <ridhamkakadiya501@gmail.com>
2025-07-18 15:36:00 +0530

    add auth route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const userRoutes = require("./userRoutes");
 const leadRoutes = require("./leadRoutes");
+const authRoutes = require("./authRoutes");
 
+router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/leads", leadRoutes);
 
